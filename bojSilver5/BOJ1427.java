65b9a09eb8fee7be900b8566c28fc08b8f49210b
jinpold <jinpold@gmail.com>
2024-12-16 11:36:13 +0900

    241216 BOJ

---
+++ bojSilver5/BOJ1427.java
@@ -0,0 +1,36 @@
+package bojSilver5;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class BOJ1427 {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String N = br.readLine();
+        br.close();
+
+
+        Integer[] array = new Integer[N.length()];
+
+        for(int i = 0; i < array.length; i++) {
+
+            array[i] = N.charAt(i) - '0';
+        }
+
+        Arrays.sort(array, Collections.reverseOrder());
+
+        for(int i = 0; i < array.length; i++) {
+            bw.write(array[i]+"");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
