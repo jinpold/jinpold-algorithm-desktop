edd0ead7eee009f96cb947a92044ef09d7482c75
jinpold <jinpold@gmail.com>
2024-12-22 14:24:55 +0900

    241222 BOJ

--- bojSilver5/BOJ1402.java
+++ bojSilver5/BOJ1402.java
@@ -1,2 +1,22 @@
-package bojSilver5;public class BOJ1402 {
+package bojSilver5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1402 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            System.out.println("yes");
+        }
+    }
+
 }
