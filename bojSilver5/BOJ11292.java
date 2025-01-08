d9a794a230152738cd711999267f8e232c24e494
jinpold <jinpold@gmail.com>
2025-01-09 00:54:43 +0900

    250109 BOJ

---
+++ bojSilver5/BOJ11292.java
@@ -0,0 +1,36 @@
+package bojSilver5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BOJ11292 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
+
+            double max = 0;
+            ArrayList<String> answer = new ArrayList<>();
+            while (n-->0) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String a = st.nextToken();
+                double b = Double.parseDouble(st.nextToken());
+                if (b < max) continue;
+                if (b > max) answer = new ArrayList<>();
+                answer.add(a);
+                max = b;
+            }
+            for (String s : answer) sb.append(s).append(' ');
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new BOJ11292().solution();
+    }
+}
